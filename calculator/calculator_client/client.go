3443c07f65e5a7bce5e2ffa4392e9f97903a8524
paedayz <pae16phasit@gmail.com>
2021-06-03 15:04:38 +0700

    created SumService with client and server

---
+++ calculator/calculator_client/client.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/go-grpc-course/calculator/calculatorpb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello I'm a client")
+
+	// connect with service
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+
+	// close connection when all of main code is run success
+	defer cc.Close()
+
+	c := calculatorpb.NewCalculatorServiceClient(cc)
+
+	doUnary(c)
+
+}
+
+func doUnary(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do a Unary RPC...")
+	req := &calculatorpb.SumRequest{
+
+		FirstNumber:  10,
+		SecondNumber: 7,
+	}
+
+	res, err := c.Sum(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Sum RPC: %v", err)
+	}
+
+	log.Printf("Response from Sum: %v", res.SumResult)
+}
---
+++ calculator/calculator_server/server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/go-grpc-course/calculator/calculatorpb"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
+	fmt.Printf("Received Sum RPC: %v\n", req)
+	firstNumber := req.FirstNumber
+	secondNumber := req.SecondNumber
+	sum := firstNumber + secondNumber
+
+	// created *SumResponse
+	res := &calculatorpb.SumResponse{
+		SumResult: sum,
+	}
+
+	return res, nil
+
+}
+
+func main() {
+	fmt.Println("Caculator Server")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Fail to listen: %v", err)
+	}
+
+	// create grpc server
+	s := grpc.NewServer()
+
+	// create GreetService
+	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("fial to serve: %v", err)
+	}
+}
